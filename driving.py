04e6c1a23773e645a9fb90086fc79d7d924e57f8
learnerpat <466260pat@gmail.com>
2019-10-01 22:51:29 -0400

    add country

---
+++ driving.py
@@ -0,0 +1,9 @@
+country = input("where are you from? ")
+country = country.upper()
+age = input("how old are you? ")
+age=int(age)
+if country == 'CHINA':
+	if age >= 18:
+		print("You can take a licence after the driving exam.")
+	else:
+		print("You can't take a licence cause your age.")
